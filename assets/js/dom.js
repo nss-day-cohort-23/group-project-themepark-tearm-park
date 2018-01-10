cc2662674088b044443957c0a91459f40a3b0661
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-10 15:06:29 -0600

    added hover shadows, cursor => pointer, changed grid to H shape

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -5,7 +5,7 @@ const $ = require("jquery");
 // prints all areas in grid with names and colors
 const displayAreaGrid = areas => {
     const areaGrid = require("../templates/area-grid.hbs");
-    areas.splice(4, 0, []);
+    areas.splice(1, 0, []);
     areas.splice(7, 0, []);
     $("#area-grid").html(areaGrid({areas}));
 };
