ea397429d0149e3aef17344f114ad2f8fb385c36
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-10 19:07:42 -0600

    toggle display of details

--- assets/js/events.js
+++ assets/js/events.js
@@ -22,5 +22,26 @@ $("#area-grid").click(function(){
     
 });
 
+$(document).on("click", ".attraction-name", function(){
+    $(".attraction-details").toggle();
+});
+
+    // (document).on("click", ".deleteCat", function () {
+    //     let catId = $(this).attr("id");
+    //     console.log("catId", catId);
+    //     deleteCat(catId)
+    //         .then(() => {
+    //             alert("Category deleted");
+    //             return getCats();
+    //         })
+    //         .then(cats => {
+    //             listCats(cats);
+    //         })
+    //         .catch(err => {
+    //             console.log("oops", err);
+    //         });
+    // });
+
+
 
 };
